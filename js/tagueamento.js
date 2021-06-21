7941a21a6df41c58c8724e7a5f01d85bb81e911b
vndp6 <vnsixdp@gmail.com>
2021-06-21 13:13:55 -0300

    testando captura evento click sem load

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -46,4 +46,4 @@ function loadNew() {
 }
 
 //document.addEventListener("DOMContentLoaded", load, false);
-document.addEventListener("DOMContentLoaded", loadNew, false);
+//document.addEventListener("DOMContentLoaded", loadNew, false);
