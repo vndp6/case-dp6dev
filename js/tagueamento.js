3ead59f704176db0d58a36901573b43a63f35b56
vndp6 <vnsixdp@gmail.com>
2021-06-21 13:08:45 -0300

    testando captura evento click

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -17,3 +17,33 @@ gtag('js', new Date());
 gtag('config', 'G-RFZ5Y45RPJ');
 
 console.log('foi');
+
+
+
+// Função para mudar o conteúdo de t2
+function modifyText() {
+  var t2 = document.getElementById("t2");
+  t2.firstChild.nodeValue = "three";
+}
+
+function modifyTextNew() {
+	console.log('modifyTextNew');
+  var vn = document.getElementById("menu-lista-download");
+  alert(vn.firstChild.nodeValue);
+  //t2.firstChild.nodeValue = "three";
+}
+
+// Função para adicionar uma espera de evento em t
+function load() {
+  var el = document.getElementById("t");
+  el.addEventListener("click", modifyText, false);
+}
+
+function loadNew() {
+	console.log('loadNew');
+  var el = document.getElementById("menu-lista-download");
+  el.addEventListener("click", modifyText, false);
+}
+
+//document.addEventListener("DOMContentLoaded", load, false);
+document.addEventListener("DOMContentLoaded", loadNew, false);
